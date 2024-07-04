b001639874a47fb0d3ced038a710d3bc803b23c2
thaaoblues <thaaoblues81@gmail.com>
2024-07-04 23:26:11 +0200

    custom svg icons

--- compile.sh
+++ compile.sh
@@ -1,8 +1,7 @@
-if test "win" = $1; then
-    var="GOARCH='amd64' GOOS='windows' CGO_ENABLED=1 CC='/usr/bin/x86_64-w64-mingw32-gcc'"
+var="GOARCH='amd64' GOOS='windows' CGO_ENABLED=1 CC='/usr/bin/x86_64-w64-mingw32-gcc'"
+build_file="qsync_windows_x64.exe"
+eval "$var go build -o $build_file main.go"
 
-else
-    var=""
-fi
-
-eval "$var go build main.go"
+var=""
+build_file="qsync_linux_x64"
+eval "$var go build -o $build_file main.go"
